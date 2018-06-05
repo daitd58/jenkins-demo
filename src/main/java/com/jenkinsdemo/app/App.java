cd554babccbba943122f203ad953748a47de77a1
Trinh Duc Dai <neverfrown.95@gmail.com>
2018-06-05 10:27:02 +0000

    uploaded project

---
+++ src/main/java/com/jenkinsdemo/app/App.java
@@ -0,0 +1,13 @@
+package com.jenkinsdemo.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
